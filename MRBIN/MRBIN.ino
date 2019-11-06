c36d46c408d252d78b9437daa9d99f430653658c
Rex Endozo <55032456+rexendz@users.noreply.github.com>
2019-11-07 00:28:54 +0800

    Actual code with height
    Removed the constant height and instead use measured height

--- MRBIN/MRBIN.ino
+++ MRBIN/MRBIN.ino
@@ -1,304 +1,188 @@
-/*  CODE       FOR
- *  INITIAL DESIGN
- *  FOR THESIS  II
- *  BY: REX ENDOZO
- *   &  JR NAVARRO
- *
-*/
 #include <LiquidCrystal_I2C.h>
-
-#define bottleHeight 20.5
-#define totalWidth 40
-#define totalLength 25
+#include <math.h>
+double totalWidth = 20;
+double totalLength = 40;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-bool debugSensors = false;
-
+// ULTRASONIC SENSORS
 const int sensor1Trig = 2;
 const int sensor1Echo = 3;
-
 const int sensor2Trig = 4;
 const int sensor2Echo = 5;
-
 const int sensor3Trig = 6;
 const int sensor3Echo = 7;
-
 const int sensor4Trig = 8;
 const int sensor4Echo = 9;
 
-const int buttonPin1 = 10;
-const int buttonPin2 = 11;
-
-unsigned long timer1 = 0;
-unsigned long timer2 = 0;
-int lastButtonReading1 = LOW;
-int lastButtonReading2 = LOW;
-int buttonState1 = LOW;
-int buttonState2 = LOW;
-
-bool viewSamples = false;
-
-double volArr[20];
-double samples[10];
+double volArray[20];
+double samples[100];
 int sampleCount = 0;
-int dispSample = 0;
 
-void setup() {
-  pinMode(buttonPin1, INPUT);
-  pinMode(buttonPin2, INPUT);
+void setup(){
+  Serial.begin(9600);
   lcd.init();
   lcd.backlight();
-  Serial.begin(9600);
-  for(int i = 1; i <= 4; i++)
-    Serial.println(getDistance(i)); // Test ultrasonic sensors
-  delay(1000);
-}
-
-void loop() {
-  if(debugSensors == true){
-    Serial.print("Sensor 1: ");
-    Serial.print(getDistance(1));
-    Serial.println("cm");
-  
-   Serial.print("Sensor 2: ");
-   Serial.print(getDistance(2));
-   Serial.println("cm");
-  
-    Serial.print("Sensor 3: ");
-    Serial.print(getDistance(3));
-   Serial.println("cm");
-  
-    Serial.print("Sensor 4: ");
-    Serial.print(getDistance(4));
-    Serial.println("cm");
-    
-   delay(500);
-  }
-  
-  else{
-    int count = 0; // Ultrasonic sensor reading count
-    double aveVol = 0; // Average volume of the object
-    bool startScan = false; // Indicates if scanning of object is initialized or not
-    bool readSuccess = false; // Indicates if scanning of object is successful or not
-    
-    while(objectDetected() && count < 500){ // First checks if there is an object inside the container
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("     OBJECT     ");
-      lcd.setCursor(0, 1);
-      lcd.print("    DETECTED!   ");
-      
-      int buttonReading1 = digitalRead(buttonPin1); // Save button reading
-      
-      if(buttonReading1 != lastButtonReading1) // If the current button reading is different from the last button reading
-        timer1 = millis(); // Sets the timer to millis
-        
-      if((millis() - timer1) > 50){ // If the time the button has been pressed exceeds 50 milliseconds
-        if(buttonReading1 != buttonState1){ // If the state of the button is different from the reading of the button
-          buttonState1 = buttonReading1; // Sets the state of the button as the current reading of the button
-          if(buttonReading1 == HIGH) // If the button is pressed down
-            startScan = true; // Set startScan to TRUE
-        }
-      }
-  
-      if(startScan == true){ // If the user pressed the button to scan
-        while(count < 20) //
-         volArr[count++] = getVolume(); // Inserts the first 20 data into the array
-      
-      if(count >= 20){
-        for(int i = 0; i < 19; i++) // Shifts the data to the left to make room for the new datum
-          volArr[i] = volArr[i+1]; 
-        
-          volArr[19] = getVolume(); // Insert the latest data into the array
-          count++;
-      }
-      
-        aveVol = 0; // Resets aveVol for recalculation
-        
-        for(int i = 0; i < 20; i++) // Adds all the data
-          aveVol += volArr[i];
-  
-      aveVol /= 20; // Divide the sum by the total number of data to get the average
-      if(count < 399){
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("    SCANNING    ");
-        lcd.setCursor(0, 1);
-        lcd.print("  PLEASE  WAIT  ");
-      }
-      if(count >= 399)
-        dispToLCD(aveVol); // Display the average volume
-        
-      if(count == 499){
-        samples[sampleCount++] = aveVol;
-        readSuccess = true;
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("    READING     ");
-        lcd.setCursor(0, 1);
-        lcd.print("    SUCCESS!!   ");
-        if(sampleCount > 9)
-          sampleCount = 0;
-        delay(5000);
-      }
-    }
-    lastButtonReading1 = buttonReading1;
-  }
-  
-  int buttonReading2 = digitalRead(buttonPin2);
-  if(buttonReading2 != lastButtonReading2)
-    timer2 = millis();
-      
-  if((millis() - timer2) > 50){
-    if(buttonState2 != buttonReading2){
-      buttonState2 = buttonReading2;
-      if(buttonState2 == HIGH){
-        if(viewSamples == false)
-          dispSample = 0;
-        else
-          dispSample++;
-         
-        viewSamples = true;
-        if(dispSample >= sampleCount)
-          viewSamples = false;
-      }
-    }
-  }
-    /*
-    for(int i = 0; i < 10; i++){
-      Serial.print("Reading ");
-      Serial.print(i+1);
-      Serial.print(": ");
-      Serial.print(samples[i]);
-      Serial.println("mL");
-    }
-    */
-    lastButtonReading2 = buttonReading2;
-
-  if(viewSamples = true){
-    if(sampleCount == 0){
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("  SAMPLES  ARE  ");
-      lcd.setCursor(0, 1);
-      lcd.print("     EMPTY!     ");
-      delay(1000);
-      viewSamples = false;
-    }
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("    SAMPLE ");
-    lcd.print(dispSample+1);
-    lcd.print("    ");
-    lcd.setCursor(0, 1);
-    lcd.print("  ");
-    lcd.print(samples[dispSample++]);
-    lcd.print("mL");
-  }
-  else{
-    if(readSuccess == false){
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("  CONTAINER IS  ");
-      lcd.setCursor(0, 1);
-      lcd.print("     EMPTY!     ");
-    }
-    delay(100);
-    }
-  }
-}
-
-void dispToLCD(double volume){
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Volume: ");
-  lcd.print(volume);
-  lcd.print("mL");
-  delay(100);
+  lcd.print("  INITIALIZING  ");
+  lcd.setCursor(0, 1);
+  lcd.print("  PLEASE  WAIT  ");
+  for(int j = 0; j < 20; j++){
+  for(int i = 1; i <= 4; i++)
+    Serial.println(getDistance(i)); // warm up the sensors
+  }
+
+  mapDimensions(); // Get the exact dimensions of the container from the perspective of the sensors
+  Serial.print("Total Length: ");
+  Serial.print(totalLength);
+  Serial.println("cm");
+  Serial.print("Total Width: ");
+  Serial.print(totalWidth);
+  Serial.println("cm");
 }
 
-bool objectDetected(){
-  double dist1 = getDistance(1);
-  double dist2 = getDistance(2);
-  double dist3 = getDistance(3);
-  double dist4 = getDistance(4);
-  if(dist1 < 23.00 && dist2 < 23.00 && dist3 < 23.00 && dist4 < 23.00)
-    return true;
+void loop(){
+  scanObject();
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("  CONTAINER IS  ");
+  lcd.setCursor(0, 1);
+  lcd.print("     EMPTY!     ");
+}
+
+void mapDimensions(){
+  double dist1, dist2, dist3, dist4;
+  for(int i = 0; i < 10; i++){
+    do{
+      dist1 = getDistance(1);
+    }while(dist1 <= 20 && dist1 > 18);
+    do{
+      dist2 = getDistance(2);
+    }while(dist2 <= 20 && dist2 > 18);
+    do{
+      dist3 = getDistance(3);
+    }while(dist3 <= 40 && dist3 > 38);
+    do{
+      dist4 = getDistance(4);
+    }while(dist4 < 40 && dist4 > 38);
+  }
+  dist1/=10;
+  dist2/=10;
+  dist3/=10;
+  dist4/=10;
+  totalWidth = (dist1+dist2)/2;
+  totalLength = (dist3+dist4)/2;
+}
+
+void scanObject(){ // Get dimensions of the object inside the container (if any)
+  int count = 0; // Number of scanning iteration
+  double aveVol = 0; // Average volume calculated of an object (Formula based on a right cylinder model: V=πr2h)
+
+  while(objectDetected() && count < 300){ // Keep on getting dimensions while there is an object in the container and count iteration is less than 300
+    if(count == 0){ // Start of scanning iteration, lets user know that scanning is in progress
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("    SCANNING    ");
+      lcd.setCursor(0, 1);
+      lcd.print("  PLEASE  WAIT  ");
+      delay(10);
+    }
+    
+    while(count < 20) // Inserts first 20 data into the array
+      volArray[count++] = getVolume();
+
+    if(count >= 20){
+      for(int i = 0; i < 19; i++) // Shifts the array to the left to make room for new datum
+        volArray[i] = volArray[i+1];
+
+      volArray[19] = getVolume(); // Insert new datum into the array
+      count++; // Increment the scanning count
+    }
+    aveVol = 0; // Set aveVol to 0 for calculation of the average volume in the array
+
+    for(int i = 0; i < 20; i++) // Sums all the 20 data from the array
+      aveVol += volArray[i];
+
+    aveVol /= 20; // Divides the sum by the total number of data to get the average volume
+
+    if(count >= 99){ // Halfway through the scanning process, show the user the current average volume reading because why not
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Volume: ");
+      lcd.print(aveVol);
+      lcd.print("mL");
+      delay(10);
+    }
+    if(count == 299){ // When the object scanning is finished
+      samples[sampleCount++] = aveVol; // Save the current reading to samples for research purposes
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("    READING     ");
+      lcd.setCursor(0, 1);
+      lcd.print("    SUCCESS!!   ");
+      delay(1000); // 5 seconds delay between scans
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("  FINAL VOLUME  ");
+      lcd.setCursor(0, 1);
+      lcd.print(aveVol);
+      delay(4000);
+    }
+  }
+}
+
+bool objectDetected(){ // Checks if there is an object in the container
+  double sensor1 = getDistance(1); // Get sensor1 distance
+  double sensor2 = getDistance(2); // Get sensor2 distance
+  if(sensor1 < (totalWidth-2) && sensor2 < (totalWidth-2)) // If there is an object blocking sensor1 and sensor2, then we can conclude that there IS an object in the container
+    return true; // Return true indicating that there is an object in the container
   else
-    return false;
+    return false; // Return false indicating that there isn't any object in the container
 }
 
-double getVolume(){
-  double sensor1 = getDistance(1);
-  double sensor2 = getDistance(2);
-  double sensor3 = getDistance(3);
-  double sensor4 = getDistance(4);
-  double d = totalLength - (((sensor1+sensor2)/2)+((sensor3+sensor4)/2));
-  double h = bottleHeight;
-  double r = d/2;
-  double vol = (3.14)*(r*r)*(h);
-
-  /*DEBUG
-    Serial.print("Diameter: ");
-    Serial.print(d);
-    Serial.println("cm");
-    Serial.print("Height: ");
-    Serial.print(h);
-    Serial.println("cm");
-  */
-  return vol;
+double getVolume(){ // VOLUME CALCULATION IS MODELED BASED ON A RIGHT CYLINDER (Which most PET bottles more or less are shaped)
+  double sensor1 = getDistance(1); // WIDTH-side distance
+  double sensor2 = getDistance(2); // WIDTH-side distance
+  double sensor3 = getDistance(3); // LENGTH-side distance
+  double sensor4 = getDistance(4); // LENGTH-side distance
+  double diameter = totalWidth - (sensor1 + sensor2); // We can get the diameter of the object by subtracting the sum of the distances of the WIDTH-side sensors from the total width
+  double radius = diameter/2; // We can then get the radius by simply dividing the diameter by 2
+  double height = totalLength - (sensor3 + sensor4); // We can get the height of the object by subtracting the sum of the distances of the LENGTH-side sensors from the total length
+  double volume = (M_PI)*(pow(radius, 2))*(height); // M_PI is a predefined constant in the math.h library which equals to the value of pi
+  return volume;
 }
 
-double getDistance(int sensor){
-  float duration; // Since the time of receiving the transmitted signal is usually in microseconds, we use float datatype to utilize alot of decimals
-  int trig, echo;
-  double distance;
-  switch (sensor){ // Selects which sensor will be active
+double getDistance(int sensor){ // Get distance using ultrasonic sensor
+  int trig, echo; // The active trig and echo pins
+  switch(sensor){ // Switch through which sensor will be used (1-4)
     case 1: trig = sensor1Trig;
             echo = sensor1Echo;
             break;
     case 2: trig = sensor2Trig;
-            echo = sensor2Echo;              
+            echo = sensor2Echo;
             break;
     case 3: trig = sensor3Trig;
             echo = sensor3Echo;
             break;
     case 4: trig = sensor4Trig;
-            echo = sensor4Echo;              
+            echo = sensor4Echo;
             break;
-    default: Serial.print("Error");
-    }
- pinMode(trig, OUTPUT); // Sets active trig pin to OUTPUT mode
- digitalWrite(trig, LOW);
- digitalWrite(echo, LOW); 
- delayMicroseconds(2);
- 
- digitalWrite(trig, HIGH); // Send a HIGH signal to trig pin for 10 microseconds
- delayMicroseconds(10); 
- digitalWrite(trig, LOW); 
- 
- pinMode(echo, INPUT); // Sets active echo pin to INPUT mode
- duration = pulseIn(echo, HIGH); // Waits for rising edge signal in echo pin and counts the duration(in uS) of which the 40khz signal is returned
- distance = duration * 340/20000; // To get the distance in cm: duration/2(us / 1000) = duration/2(ms) * 340m/s = meters/100 = centimeters
- delayMicroseconds(100); // We divide the duration by 2 as the time is doubled with the process of transmitting->reflect->receiving (RADAR principle)
- return distance; // Gives the distance value to whoever called this function
+    default: Serial.println("Error: Out of bounds");
+  }
+  pinMode(trig, OUTPUT); // Set the active trig pin as OUTPUT
+  digitalWrite(trig, LOW);
+  digitalWrite(echo, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trig, HIGH); // Send out a 10 microsecond pulse in the trig pin
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  
+  pinMode(echo, INPUT);
+  double duration = pulseIn(echo, HIGH); // Count the time it took for the echo to detect the rising edge of the pulse
+  double distance = duration * 340/20000; // Calculate the distance in centimeters
+  delayMicroseconds(100); // Delay between scans
+  return distance;
 }
-
-/* NOT USED
-int getAverageDistance(int sensor){
-  int totalDist;
-  int currDist;
-  totalDist = 0;
-    for(int i= 0; i < 10; i++){
-      do{
-      currDist = getDistance(sensor);
-      Serial.println(currDist);
-      }while(currDist < 2 || currDist > 26);
-      totalDist += currDist;
-      delay(1);
-}
-  totalDist /= 10;
-  return totalDist;
-}
-*/
