960be5f10c6419830a3b3f9a54e39db4c4e9425b
rexendz <rexendozo@gmail.com>
2019-12-04 23:16:38 +0800

    Combined dist-serial and rfid-serial to one arduino file, mrbin.ino Integrated sql to main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from camproc import processing
+from sql import SQLServer
+import time
 import argparse
 try:
     from arduino import SerialListener
-except ImportError or SerialException:
-    print ("Warning: No Arduino connected")
+except ImportError or ImportError:
+    print("Warning: No Arduino connected")
 
 if __name__ == "__main__":
     object_detected = False
-    window_started = False
     
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--ip", help="Select IP Camera with URL Instead Of PI Camera")
@@ -17,27 +18,60 @@ if __name__ == "__main__":
     
     device = "__PI__"
     url = "0.0.0.0"
+    window = 4;
     
     if args.ip is not None:
         device = "__IP__"
         url = args.ip
-    
+    if args.window is not None:
+        window = args.window
+
     try:
         reader = SerialListener().start()
-        while (reader.read()) == -1:
-            pass
     except NameError:
-        print ("Warning: No Arduino")
+        print("Warning: No Arduino")
 
     print("Done!")
+
+    sql = SQLServer()
+
+    reader.flush()
+    userAuthenticated = False
+    name = None
+    pts = None
+    while not userAuthenticated:
+        uid = ''
+        while uid is '':
+            uid = reader.readRFID()
+        time.sleep(0.1)
+        user = sql.findUid(int(uid, 16))
+        if len(user) > 0:
+            (_, name, _, pts), = user
+            print("Welcome, " + name)
+            print("Current Incentives: " + str(pts))
+            reader.write('O')
+            userAuthenticated = True
+        else:
+            reader.write('X')
+            print("There is no record of UID: " + uid + " in our database")
+            cr = input("Would you like to create a new one? (Y/N): ")
+            if cr == 'Y' or cr == 'y':
+                name = input("Enter your name: ")
+                sql.insert(name, int(uid, 16), 0)
+            print("Please scan your ID again")
+        time.sleep(1)
+
+    sql.close()
+
     processor = processing(device=device, url=url)
+
     while True:
         try:
-            distance = reader.read()
+            distance = reader.readDistance()
             if distance <= 10 and distance > 2:
                 if not object_detected:
                     print("Object Detected!")
-                    print("Distance from sensor: ", reader.read())
+                    print("Distance from sensor: ", reader.readDistance())
                     object_detected = True
             
             elif distance > 10:
@@ -51,11 +85,11 @@ if __name__ == "__main__":
             
         elif object_detected:
             # window=0 --- Raw Image from Camera
-            # window=1 --- Image w/ Detection 
+            # window=1 --- Image w/ Detection
             # window=2 --- Edge Mask
             # window=3 --- Image w/ Detection & Edge Mask
             # window=4 --- Image w/ Detection & Edge Mask & Trackbars
-            k = processor.display_proc(window=args.window)
+            k = processor.display_proc(window=window)
             
             if k == 27:
                 break
