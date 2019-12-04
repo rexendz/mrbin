7c7485fdb7b0abc5dbaf4adbf627e313ab8ec0c2
rexendz <rexendozo@gmail.com>
2019-12-04 19:59:53 +0800

    SQL Connection added and RFID now checks database

--- arduino.py
+++ arduino.py
@@ -1,21 +1,26 @@
 # Reads data from the serial monitor without interrupting the main thread
 
-from serial import Serial
+from serial import Serial, serialutil
 import time
 from threading import Thread
+from sql import SQLServer
 
 
 class SerialListener:
     def __init__(self, baudrate=9600, timeout=0.5):
         try:
             self.ser = Serial('/dev/ttyACM0', baudrate, timeout=timeout)
-        except:
-            self.ser = Serial('/dev/ttyACM1', baudrate, timeout=timeout)
+        except serialutil.SerialException:
+            try:
+                self.ser = Serial('/dev/ttyACM1', baudrate, timeout=timeout)
+            except serialutil.SerialException:
+                self.ser = Serial('/dev/ttyACM2', baudrate, timeout=timeout)
 
         self.stopped = False
         self.paused = False
         self.stream = ''
         time.sleep(1)  # Wait for serial buffer to reset
+        print("Serial Started")
 
     def start(self):
         Thread(target=self.update, args=()).start()
@@ -62,19 +67,30 @@ class SerialListener:
 
 if __name__ == "__main__":  # FOR DEBUGGING ONLY
     uno = SerialListener().start()
+    sql = SQLServer()
     uno.flush()
-    print("Serial Started")
+    name = None
+    pts = None
 
     while True:
         uid = ''
         while uid is '':
             uid = uno.readRFID()
         time.sleep(0.1)
-
-        if uid == "5BEE9F0D":
+        user = sql.findUid(int(uid, 16))
+        if len(user) > 0:
+            (_, name, _, pts), = user
+            print("Welcome, " + name)
+            print("Current Incentives: " + str(pts))
             uno.write('O')
         else:
             uno.write('X')
+            print("There is no record of UID: " + uid + " in our database")
+            cr = input("Would you like to create a new one? (Y/N): ")
+            if cr == 'Y' or cr == 'y':
+                name = input("Enter your name: ")
+                print(sql.insert(name, int(uid, 16), 0), "record added")
+            print("Please scan your ID again")
         print(uid)
         time.sleep(1)
 
---
+++ sql.py
@@ -0,0 +1,87 @@
+import pymysql
+
+class SQLServer:
+    def __init__(self, host="localhost", user="root", passwd="", database="mrbin"):
+        self.conn = pymysql.connect(
+            host=host, 
+            user=user, 
+            passwd=passwd, 
+            database=database)
+        self.query = ''
+        self.curr = self.conn.cursor()
+
+    def getLength(self):
+        return len(self.readAll())
+
+    def insert(self, name, uid, pts, table="Users"):
+        self.query = """
+        INSERT INTO {} (Name, RFID_UID, Incentives) VALUES ('{}', {}, {})
+        """.format(table, name, uid, pts)
+        self.execute()
+        self.commit()
+        print(self.curr.rowcount, "record inserted")
+
+    def update(self, Where, WhereVal, Set, SetVal, table="Users"):
+        self.query= """
+        UPDATE {}
+        SET {} = {}
+        WHERE {} = {}
+        """.format(table, Set, SetVal, Where, WhereVal)
+        self.execute()
+        self.commit()
+        print(self.curr.rowcount, "record updated")
+
+    def updateIncrement(self, table="Users"):
+        nextID = self.getLength() + 1
+        self.query = "ALTER TABLE {} AUTO_INCREMENT = {}".format(table, nextID)
+        self.execute()
+        self.commit()
+
+    def delete(self, Where, WhereVal, table="Users"):
+        if Where == "Name":
+            self.query = "DELETE FROM {} WHERE {} = '{}'".format(table, Where, WhereVal)
+        else:
+            self.query = "DELETE FROM {} WHERE {} = {}".format(table, Where, WhereVal)
+        self.execute()
+        self.commit()
+        print(self.curr.rowcount, "record deleted")
+        self.updateIncrement(table)
+
+    def deleteLatest(self):
+        self.delete("ID", self.getLength())
+
+    def execute(self):
+        self.curr.execute(self.query)
+
+    def getResult(self):
+        return self.curr.fetchall()
+    
+    def commit(self):
+        self.conn.commit()
+
+    def readAll(self, table="Users"):
+        self.query = "SELECT * FROM {}".format(table)
+        self.execute()
+        return self.curr.fetchall()
+
+    def findUid(self, uid, table="Users"):
+        self.query = """
+        SELECT * FROM {} WHERE RFID_UID = {}
+        """.format(table, uid)
+        self.execute()
+        return self.curr.fetchall()
+
+    def uidIsExisting(self, uid):
+        return len(self.findUid(uid))
+
+    def close(self):
+        self.conn.close()
+    
+
+if __name__ == "__main__":
+    sql = SQLServer("localhost", "root", passwd="", database="mrbin")
+    for rows in sql.findUid(int(0x563D0346)):
+        _, name, uid, pts = rows
+    print("Welcome, " + name)
+    print("Points: " + str(pts))
+    sql.close()
