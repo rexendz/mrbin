749af5e062179f913ef82498a112e521c18907aa
rexendz <rexendozo@gmail.com>
2019-12-08 02:40:15 +0800

    Added semi-working GUI for MRBIN using PyQt5

---
+++ AboutGUI.py
@@ -0,0 +1,96 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import sys
+
+
+class AnimationLabel(QLabel):
+    def __init__(self, *args, **kwargs):
+        QLabel.__init__(self, *args, **kwargs)
+        self.animation = QVariantAnimation()
+        self.animation.valueChanged.connect(self.changeColor)
+
+    @pyqtSlot(QVariant)
+    def changeColor(self, color):
+        palette = self.palette()
+        palette.setColor(QPalette.WindowText, color)
+        self.setPalette(palette)
+
+    def startFadeIn(self):
+        self.animation.stop()
+        self.animation.setStartValue(QColor(255, 255, 255, 0))
+        self.animation.setEndValue(QColor(255, 255, 255, 255))
+        self.animation.setDuration(2000)
+        self.animation.setEasingCurve(QEasingCurve.InBack)
+        self.animation.start()
+
+    def startFadeOut(self):
+        self.animation.stop()
+        self.animation.setStartValue(QColor(255, 255, 255, 255))
+        self.animation.setEndValue(QColor(255, 255, 255, 0))
+        self.animation.setDuration(2000)
+        self.animation.setEasingCurve(QEasingCurve.OutBack)
+        self.animation.start()
+
+    def startAnimation(self):
+        self.startFadeIn()
+        loop = QEventLoop()
+        self.animation.finished.connect(loop.quit)
+        loop.exec_()
+        QTimer.singleShot(2000, self.startFadeOut)
+
+
+class About(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.title = "MR BIN"
+        self.top = 264
+        self.left = 523
+        self.width = 320
+        self.height = 240
+        self.icon = QIcon('/home/rexendz/mrbin/res/favicon.png')
+        self.vbox = QVBoxLayout()
+
+        self.InitWindow()
+        self.InitComponents()
+
+    def InitWindow(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setWindowIcon(self.icon)
+        self.setStyleSheet("background-color: #212121;")
+        self.setWindowFlags(Qt.FramelessWindowHint)
+
+        self.setLayout(self.vbox)
+
+    def InitComponents(self):
+        label1 = AnimationLabel("Machine for Recycling Bottles\nwith\nIncentive Noting\n\n(MR BIN)\n")
+        label1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        label1.setAlignment(Qt.AlignCenter)
+        label1.setStyleSheet("font : 20px; font : bold; font-family : Sanserif;")
+
+        label2 = AnimationLabel("Created By\n\nRex Christian R. Endozo\n\nand\n\nJohn Ray F. Navarro")
+        label2.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        label2.setAlignment(Qt.AlignCenter)
+        label2.setStyleSheet("font : 20px; font : bold; font-family : Sanserif;")
+        label1.hide()
+        label2.hide()
+
+        self.vbox.addWidget(label1)
+        self.vbox.addWidget(label2)
+
+        label1.show()
+        label1.startFadeIn()
+
+        QTimer.singleShot(5000, label1.startFadeOut)
+        QTimer.singleShot(6000, label1.hide)
+        QTimer.singleShot(6000, label2.show)
+        QTimer.singleShot(6000, label2.startFadeIn)
+        QTimer.singleShot(10000, label2.startFadeOut)
+        QTimer.singleShot(11000, label2.hide)
+        QTimer.singleShot(11000, self.close)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = About()
+    sys.exit(app.exec())
---
+++ LoginGUI.py
@@ -0,0 +1,102 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import sys
+
+
+class customLineEdit(QLineEdit):
+    def focusInEvent(self, QFocusEvent):
+        if self.text() == 'Username':
+            self.setText('')
+            self.setStyleSheet("color : white;")
+        if self.text() == 'Password':
+            self.setText('')
+            self.setEchoMode(QLineEdit.Password)
+            self.setStyleSheet("color : white;")
+
+    def focusOutEvent(self, QFocusEvent):
+        if self.text() == '' and self.objectName() == 'User':
+            self.setText('Username')
+            self.setStyleSheet("color : gray;")
+        if self.text() == '' and self.objectName() == 'Pass':
+            self.setText('Password')
+            self.setEchoMode(QLineEdit.Normal)
+            self.setStyleSheet("color : gray;")
+
+
+class Login(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.title = "MR BIN"
+        self.left = 523
+        self.top = 264
+        self.width = 320
+        self.height = 240
+        self.icon = QIcon('/home/rexendz/mrbin/res/favicon.png')
+        self.vbox = QVBoxLayout()
+        self.usr = None
+        self.pwd = None
+        self.InitWindow()
+        self.InitComponents()
+
+        self.show()
+
+    def InitWindow(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setMaximumHeight(self.height)
+        self.setMaximumWidth(self.width)
+        self.setMinimumHeight(self.height)
+        self.setMinimumWidth(self.width)
+        self.vbox.setGeometry(QRect(self.left, self.top, self.width, self.height))
+        self.vbox.setSpacing(1)
+        self.setStyleSheet("background-color: #212121;")
+        self.setWindowIcon(self.icon)
+        self.setLayout(self.vbox)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+
+    def InitComponents(self):
+        self.usr = customLineEdit("Username", self)
+        self.pwd = customLineEdit("Password", self)
+        self.usr.setStyleSheet("color : gray;")
+        self.pwd.setStyleSheet("color : gray;")
+        btn1 = QPushButton("Login", self)
+        btn2 = QPushButton("Back", self)
+
+        self.usr.setObjectName("User")
+        self.pwd.setObjectName("Pass")
+
+        btn1.clicked.connect(self.btn1Action)
+        btn2.clicked.connect(self.btn2Action)
+
+        vbox1 = QVBoxLayout()
+        vbox2 = QVBoxLayout()
+
+        self.vbox.addLayout(vbox1)
+        self.vbox.addLayout(vbox2)
+
+        vbox1.addWidget(self.usr)
+        vbox1.addWidget(self.pwd)
+        vbox2.addWidget(btn1)
+        vbox2.addWidget(btn2)
+
+    def btn1Action(self):
+        msg = QMessageBox()
+        if self.usr.text() == '' or self.pwd.text() == '' or (self.usr.text() == 'Username' and self.pwd.text() == 'Password'):
+            msg.warning(self, "Login Error", "Empty Fields!\nFill out the fields.")
+        else:
+            if str(self.usr.text()) == "admin" and str(self.pwd.text()) == "12345":
+                msg.information(self, "Login Success", "Login Successful!\nAdministator mode activated.")
+            else:
+                msg.warning(self, "Login Error", "Wrong credentials")
+
+        print(self.usr.text(), self.pwd.text())
+
+    def btn2Action(self):
+        self.close()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Scan()
+    app.exec()
