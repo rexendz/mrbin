ea7515ca12f316d1543d44ce2b3687c601f1172f
rexendz <rexendozo@gmail.com>
2019-12-13 03:15:37 +0800

    Added camera gui opencv integration

--- CameraGUI.py
+++ CameraGUI.py
@@ -1,7 +1,31 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+import cv2
+import numpy as np
 import sys
+
+
+class camera(QThread):
+    changePixmap = pyqtSignal(QImage)
+
+    def __init__(self, device, url, parent=None):
+        super().__init__(parent)
+        self.device = device
+        self.url = url
+        self.stopped = False
+
+    def run(self):
+        cap = cv2.VideoCapture(self.url)
+        while not self.stopped:
+            ret, frame = cap.read()
+            if ret:
+                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                h, w, ch = rgbImage.shape
+                bytesPerLine = ch * w
+                convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
+                p = convertToQtFormat.scaled(300, 300, Qt.KeepAspectRatio)
+                self.changePixmap.emit(p)
 
 
 class Cam(QDialog):
@@ -12,12 +36,19 @@ class Cam(QDialog):
         self.title = "MR BIN"
         self.left = 0
         self.top = 0
-        self.width = 320
-        self.height = 240
+        self.width = 480
+        self.height = 320
         self.icon = QIcon('/home/rexendz/mrbin/res/favicon.png')
+        self.gbox = QGridLayout()
         self.vbox = QVBoxLayout()
         self.name = name
         self.pts = pts
+        self.pic = None
+        self.device = device
+        self.ip = url
+        self.th = camera(self.device, self.ip,parent=self)
+        self.th.changePixmap.connect(self.setImage)
+        self.th.start()
         self.InitWindow()
         self.InitComponents()
 
@@ -30,30 +61,42 @@ class Cam(QDialog):
         self.setMaximumWidth(self.width)
         self.setMinimumHeight(self.height)
         self.setMinimumWidth(self.width)
-        self.vbox.setGeometry(QRect(self.left, self.top, self.width, self.height))
-        self.vbox.setSpacing(1)
-        self.setStyleSheet("background-color: #212121;")
+        self.gbox.setGeometry(QRect(self.left, self.top, self.width, self.height))
+        self.gbox.setSpacing(1)
+        self.setStyleSheet("background-color: #297045;")
         self.setWindowIcon(self.icon)
         self.setLayout(self.vbox)
         self.setWindowFlags(Qt.FramelessWindowHint)
 
+    def setImage(self, image):
+        self.pic.setPixmap(QPixmap.fromImage(image))
+
     def InitComponents(self):
-        lbl1 = QLabel(self.name, self)
-        lbl2 = QLabel(str(self.pts), self)
+        lbl1 = QLabel(("User: " + self.name), self)
+        lbl2 = QLabel(("Current Incentives: " + str(self.pts)), self)
+        self.pic = QLabel(self)
+        self.pic.resize(300, 300)
 
         btn1 = QPushButton("Exit", self)
+        lbl1.setAlignment(Qt.AlignLeft)
+        lbl2.setAlignment(Qt.AlignRight)
+        lbl1.setStyleSheet("font : 20px; font-family : Sanserif; color : #e1efe6")
+        lbl2.setStyleSheet("font : 20px; font-family : Sanserif; color : #e1efe6")
 
         btn1.clicked.connect(self.btn1Action)
-
-        self.vbox.addWidget(lbl1)
-        self.vbox.addWidget(lbl2)
+        btn1.setStyleSheet("background-color : #aeb7b3; color : #1b2f33; font : 20px; font-family : Sanserif;")
+        self.vbox.addLayout(self.gbox)
+        self.gbox.addWidget(lbl1, 0, 0)
+        self.gbox.addWidget(lbl2, 0, 1)
+        self.vbox.addWidget(self.pic)
         self.vbox.addWidget(btn1)
 
     def btn1Action(self):
+        self.close()
         self.switch_back.emit(self)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = Cam("amaze", "959")
+    window = Cam("__IP__", "http://192.168.1.2:8080/video", "amaze", "959")
     app.exec()
---
+++ temp/scrabble.py
@@ -0,0 +1,84 @@
+import numpy as np 
+import cv2
+import imutils
+import time
+from scipy.spatial import distance as dist
+from imutils import contours, perspective
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+
+class imageprocessing:
+    def smoothImage(self, img):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (7, 7), 0)
+        return gray
+
+    def getCanny(self, img, lth, uth):
+        edged = cv2.Canny(img, lth, uth)
+        edged = cv2.dilate(edged, None, iterations=1)
+        edged = cv2.erode(edged, None, iterations=1)
+        return edged
+    
+    def getContours(self, img):
+        cnts = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        if len(cnts) > 1:
+            (cnts, _) = contours.sort_contours(cnts)
+            return cnts
+        else:
+            return -1  # Which means there are no contours
+
+    def getValidContour(self, cnts, lth, uth):
+        for c in cnts:
+            if cv2.contourArea(c) > lth or cv2.contourArea(c) < uth:
+                return c
+
+    def getPoints(self, contour):
+        rect = cv2.minAreaRect(contour)
+        box = cv2.boxPoints(rect)
+        box = perspective.order_points(box) # Order 4 points to top left, top right, bottom right, bottom left
+        return box
+
+    def drawConts(self, img, cnts):
+        cv2.drawContours(img, [cnts.astype("int")], -1, (0, 255, 0), 2)
+        
+    def showImage(self, img):
+        cv2.imshow('Board', img)
+        cv2.waitKey()
+
+class scrabble(imageprocessing):
+    def __init__(self):
+        self.cam = PiCamera()
+        self.cam.resolution = (480, 320)
+        self.cam.framerate = 32
+        self.rawCapture = PiRGBArray(self.cam, size=(480, 320))
+        self.stream = self.cam.capture_continuous(self.rawCapture, format="bgr", use_video_port=True)
+        self.img = None
+
+
+    def getBoard(self):
+        for f in self.stream:
+            self.img = f.array
+            image = self.smoothImage(self.img)
+            image = self.getCanny(image, 50, 100)
+            image_contour = self.getContours(image)
+            valid = self.getValidContour(image_contour, 30000, 40000)
+            board_pts = self.getPoints(valid)
+            if board_pts is not None:
+                return board_pts
+            self.rawCapture.truncate(0)
+
+    def drawBoard(self, box):
+        self.drawConts(self.img, box)
+
+if __name__ == "__main__":
+    game = scrabble()
+    board_pts = game.getBoard()
+    game.drawBoard(board_pts)
+
+    (tl, tr, br, bl) = board_pts
+
+
+
+
+    
